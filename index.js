079836f2b52e0d78ffbe3682552bbc43c1fa0f78
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-23 18:42:38 -0800

    Show using fs to read from package.json
    You don't need to npm install anything to use the file-system package
    fs.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 const stackname = require('@cdk-turnkey/stackname');
 console.log(stackname('hi'));
+const fs = require('fs');
+const pkg = fs.readFileSync('./package.json', 'utf-8');
+console.log(pkg);
+const ob = JSON.parse(pkg);
+console.log(ob.name);
